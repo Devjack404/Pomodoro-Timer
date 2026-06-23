8ba88ce5e52a188d06392cb99ace3fe2f9659439
Devjack404 <juansabarofek@gmail.com>
2026-06-23 23:34:48 +0700

    feat: implement home route and add login endpoint in server.ts; create index.js and users.js route files

--- backend/server.ts
+++ backend/server.ts
@@ -3,9 +3,13 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.get('/home', (req, res) => {
+  
+})
+
+app.post('/login', (req, res) => {
+  res.send('') 
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
