c98827a1cd7c3c1a13ebd4b2812ce5caa3a4009e
Devjack404 <juansabarofek@gmail.com>
2026-06-28 23:55:08 +0700

    update folder path

---
+++ src/backend/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome Home");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login Page");
+});
+
+app.get("/register", (req, res) => {
+  res.send("fitur segera dibuat");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
